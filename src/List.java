fad3b8daaa9e2187d100cae4895e125f2c0824e7
smalex <alexdate@gmail.com>
2019-11-08 17:20:10 +0300

    anything

--- src/List.java
+++ src/List.java
@@ -1,2 +1,23 @@
-public class List extends Vector {
+public class List<E> extends Vector {
+    public int size;
+
+    public void add(E item){
+        int i = size;
+        if (obj.length <= size){
+            expand();
+        }
+        obj[i] = (E) item;
+        size++;
+    }
+
+    public void remove(int indx){
+        E[] tmp = (E[]) new Object[size-1];
+        System.arraycopy(obj, 0, tmp, 0, indx);
+        System.arraycopy(obj, indx + 1, tmp, indx, size - indx - 1);
+        obj = tmp;
+    }
+
+    public Object[] toArray(){
+        return obj;
+    }
 }
